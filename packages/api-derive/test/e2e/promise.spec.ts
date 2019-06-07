ca631268caf361a07b13287f03fa2142f349330f
Jaco Greeff <jacogr@gmail.com>
2019-06-07 12:13:30 +0200

    Support more consensus engines with digest info (#974)
    * Support more consensus engines with digest info
    
    * Tested against both v1 & v2
    
    * Skip e2e
    
    * Apply map suggestion (more effective as well)

--- packages/api-derive/test/e2e/promise.spec.ts
+++ packages/api-derive/test/e2e/promise.spec.ts
@@ -10,8 +10,8 @@ import ApiPromise from '@plugnet/api/promise/Api';
 import testKeyring from '@plugnet/keyring/testing';
 import { WsProvider } from '@plugnet/rpc-provider';
 
-// const WS = 'ws://127.0.0.1:9944/';
-const WS = 'wss://poc3-rpc.polkadot.io/';
+const WS = 'ws://127.0.0.1:9944/';
+// const WS = 'wss://poc3-rpc.polkadot.io/';
 
 describe.skip('derive e2e', () => {
   let api: ApiPromise;
