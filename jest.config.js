1b9011220a0b0ae9a088905c99f962c60c816bc3
Jaco Greeff <jacogr@gmail.com>
2019-06-14 13:48:53 +0200

    Create api-contract (baseline for useful interactions) (#1007)
    * Create api-contract (baseline for useful interactions)
    
    * Add CHANGELOG entry (breaking)

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ const config = require('@plugnet/dev/config/jest');
 
 module.exports = Object.assign({}, config, {
   moduleNameMapper: {
+    '@plugnet/api-contract(.*)$': '<rootDir>/packages/api-contract/src/$1',
     '@plugnet/api-derive(.*)$': '<rootDir>/packages/api-derive/src/$1',
     '@plugnet/api(.*)$': '<rootDir>/packages/api/src/$1',
     '@plugnet/rpc-(core|provider|rx)(.*)$': '<rootDir>/packages/rpc-$1/src/$2',
@@ -13,6 +14,7 @@ module.exports = Object.assign({}, config, {
   modulePathIgnorePatterns: [
     '<rootDir>/packages/api/build',
     '<rootDir>/packages/api-derive/build',
+    '<rootDir>/packages/api-contract/build',
     '<rootDir>/packages/rpc-core/build',
     '<rootDir>/packages/rpc-provider/build',
     '<rootDir>/packages/rpc-rx/build',
