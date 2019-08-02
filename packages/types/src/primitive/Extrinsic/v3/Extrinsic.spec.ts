e8bd6feba5b2c21c00ed8f557fcab71ed7957b0b
Jaco Greeff <jacogr@gmail.com>
2019-08-02 21:22:01 +0200

    Bump deps (#1259)
    * Bump deps
    
    * Bump common

--- packages/types/src/primitive/Extrinsic/v3/Extrinsic.spec.ts
+++ packages/types/src/primitive/Extrinsic/v3/Extrinsic.spec.ts
@@ -2,8 +2,8 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-import extrinsics from '@polkadot/api-metadata/extrinsics/static';
-import testingPairs from '@polkadot/keyring/testingPairs';
+import extrinsics from '@plugnet/api-metadata/extrinsics/static';
+import testingPairs from '@plugnet/keyring/testingPairs';
 
 import Call from '../../Generic/Call';
 import Extrinsic from './Extrinsic';
